e8c837127166a052628c059a1713440215599db6
makiintoch <makiintoch@gmail.com>
2019-11-04 11:38:03 +0200

    replacing pages

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Filters!'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Text('Filters!'),
+    );
   }
 }
